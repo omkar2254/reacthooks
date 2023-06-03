d711725aadc9ddccc72c21831aea69210469cf98
Omkar <omkarpatil2254@gmail.com>
2023-06-03 09:54:18 +0530

    first hook

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
+import React, { useState, useEffect, useRef } from 'react'
 import './App.css';
 
 function App() {
+
+  const [name, setName] = useState('')
+  const renderCount = useRef(0)
+
+  useEffect (()=>{
+    renderCount.current = renderCount.current + 1;
+  })
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <input value={name} onChange={ e => setName(e.target.value)} />
+    <div>My name is {name}</div>
+    <p>I rendered {renderCount.current} times</p>
+    </>
   );
 }
 
