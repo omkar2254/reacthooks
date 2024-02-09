8411519ad34461178f016f5059f4d3d4c48ce85c
Omkar <omkarpatil2254@gmail.com>
2024-02-09 19:30:56 +0530

    Hooks

--- src/Components/UseCallback.js
+++ src/Components/UseCallback.js
@@ -3,18 +3,33 @@
 //The useCallback Hook only runs when one of its dependencies update.
 import { useState, useCallback } from "react"
 import Child from "./Child";
+// function UseCallback(){
+//     const[count, setCount] = useState(0)
+//     const Learning = useCallback(()=>{
+//         //some operation
+//         console.log("Learning Complete")
+//     },[])
+//     return(
+//         <>
+//         <p>Count: {count}</p>
+//         <Child Learning={Learning}/>
+//         <button onClick={()=> setCount(count+1)}>Button</button>
+//         </>
+//     );
+// }
+
+// export default UseCallback;
+
 function UseCallback(){
-    const[count, setCount] = useState(0)
+    const[count, setCount] = useState(0);
     const Learning = useCallback(()=>{
         //some operation
-    },[])
+    }, [])
     return(
         <>
         <p>Count: {count}</p>
         <Child Learning={Learning}/>
         <button onClick={()=> setCount(count+1)}>Button</button>
         </>
-    );
+    )
 }
-
-export default UseCallback;
